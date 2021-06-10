7ddf4d135bb0a8ad4fdfa4279ece2cbd889db086
Tim <s80352@beuth-hochschule.de>
2021-06-10 23:11:29 +0200

    fix: remove webbrowser crate, replace with open

--- src/repo.rs
+++ src/repo.rs
@@ -1,4 +1,4 @@
-use webbrowser;
+use open;
 
 pub fn repo(json: &serde_json::Value) {
   let repository = get_repository_from_json(json);
@@ -11,17 +11,19 @@ pub fn repo(json: &serde_json::Value) {
   };
 
   let result_string = repository.as_str();
+  match result_string {
+    Some(result) => {
+      open_browser(string_to_static_str(result.to_string()));
+    }
+    None => {
+      let url = find_url_in_repository(repository);
 
-  if result_string != None {
-    open_browser(result_string.unwrap());
-  } else {
-    let url = find_url_in_repository(repository);
-
-    match url {
-      Some(url) => open_browser(&url),
-      _ => return,
-    };
-  }
+      return match url {
+        Some(url) => open_browser(string_to_static_str(url)),
+        _ => return,
+      };
+    }
+  };
 
   return;
 }
@@ -58,8 +60,12 @@ fn find_url_in_repository(repository: &serde_json::Value) -> std::option::Option
   };
 }
 
-fn open_browser(url: &str) {
-  if webbrowser::open(url).is_ok() {
-    println!("Opening browser...");
+fn string_to_static_str(s: String) -> &'static str {
+  Box::leak(s.into_boxed_str())
+}
+
+fn open_browser(url: &'static str) {
+  if open::that(url).is_ok() {
+    println!("Opening browser with {}", url);
   }
 }
